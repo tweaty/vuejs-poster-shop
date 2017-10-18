bc715d7c52c42ea0964c6a665416a12c6acda470
Tweaty <tweatypl@gmail.com>
2017-10-19 00:42:07 +0200

    added vue-resource

--- public/script.js
+++ public/script.js
@@ -1,1 +1,61 @@
-console.log('It works.');
+var PRICE = 9.99;
+
+new Vue({
+    el: "#app",
+    data: {
+        total: 0,
+        items: [
+            { id: 1, title: 'Item 1' },
+            { id: 2, title: 'Item 2' },
+            { id: 3, title: 'Item 3' }
+        ],
+        cart: [],
+        search: ''
+    },
+    methods: {
+        onSubmit: function() {
+            console.log(this.$http);
+        },
+        addItem: function(index) {
+            this.total += 9.99;
+            var item = this.items[index];
+            var found = false;
+            for (var i = 0; i < this.cart.length; i++){
+                if (this.cart[i].id === item.id){
+                    found = true;
+                    this.cart[i].qty++;
+                    break;
+                }
+            }
+            if(!found){
+                this.cart.push({
+                    id: item.id,
+                    title: item.title,
+                    qty: 1,
+                    price: PRICE
+                });
+            }   
+        },
+        inc: function(item) {
+            item.qty++;
+            this.total += PRICE;
+        },
+        dec: function(item) {
+            item.qty--;
+            this.total -= PRICE;
+            if (item.qty <= 0) {
+                for (var i = 0; i < this.cart.length; i++) {
+                    if (this.cart[i].id === item.id){
+                        this.cart.splice(i, 1);
+                        break;
+                    }
+                }
+            }
+        }
+    },
+    filters: {
+        currency: function(price) {
+            return '$'.concat(price.toFixed(2));
+        }
+    }
+});
